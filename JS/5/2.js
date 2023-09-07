c0ec88cddadb0d8ef46fad204f9b387566f66ce4
Sjossa <johnny.sass2001@gmail.com>
2023-09-07 11:28:16 +0200

    nouveau dossier

---
+++ JS/5/2.js
@@ -0,0 +1,19 @@
+let mois=new Array(12);
+
+     mois[1]="janvier";
+     mois[2]="Fevrier";
+     mois[3]="Mars";
+     mois[4]="Avril";
+     mois[5]="mai";
+     mois[6]="juin";
+     mois[7]="juillet";
+     mois[8]="aout";
+     mois[9]="septembre";
+     mois[10]="octobre";
+     mois[11]="novembre";
+     mois[12]="decembre";
+
+     console.log(mois);
+     for(i=0; i<=mois.length;i++){
+        console.log("parcourir le tableau avec une boucle" +mois[i])
+     }
